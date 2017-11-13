68a791cd858abd82e70145855c971df4875287bb
buschor <ronny.buschor@adcubum.com>
2017-11-13 14:03:23 +0100

    Test RBS

--- player.php
+++ player.php
@@ -2,18 +2,28 @@
 
 class Player
 {
-    const VERSION = "FunkSoBrothers 0.9";
+  const VERSION = "FunkSoBrothers 0.9";
 
-    public function betRequest($game_state)
-    {
-        //if ($game_state->round == 0) {
-        //  return 10;          
-        //}
-        //else
-        return $game_state->current_buy_in + 1;
+  public function betRequest($game_state)
+  {
+    //if ($game_state->round == 0) {
+    //  return 10;          
+    //}
+    //else
+    
+  
+    for ($game_state->players as $ply) {
+      if (count($ply->hole_cards) > 0) {
+        $iam = $ply;
+      }
     }
+    fwrite(STDERR, serialize($game_state));
+    
+    return 200 //$game_state->current_buy_in + 1;
+    
+  }
 
-    public function showdown($game_state)
-    {
-    }
+  public function showdown($game_state)
+  {
+  }
 }
